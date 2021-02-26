377329a32a50f4c3255e9d16d5e991e50fc25e1b
Poccardl <loic.poccard@gmail.com>
2021-02-26 13:59:22 +0100

    fix [Bug report] - Bills

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -1,8 +1,8 @@
 import VerticalLayout from './VerticalLayout.js'
 import ErrorPage from "./ErrorPage.js"
 import LoadingPage from "./LoadingPage.js"
-
 import Actions from './Actions.js'
+import Toolbox from '../app/Toolbox.js'
 
 const row = (bill) => {
   return (`
@@ -16,15 +16,16 @@ const row = (bill) => {
         ${Actions(bill.fileUrl)}
       </td>
     </tr>
-    `)
+    `) // TODO: correction here -> ${Actions(bill.fileUrl)}
   }
 
 const rows = (data) => {
+  data = Toolbox.sortDateOnBillsTab(data)
   return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
 }
 
 export default ({ data: bills, loading, error }) => {
-  
+
   const modal = () => (`
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -47,7 +48,7 @@ export default ({ data: bills, loading, error }) => {
   } else if (error) {
     return ErrorPage(error)
   }
-  
+
   return (`
     <div class='layout'>
       ${VerticalLayout(120)}

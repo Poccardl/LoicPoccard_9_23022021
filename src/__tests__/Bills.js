d2368d640d504630e4c4f6cf17938c9e28b2de0e
Poccardl <loic.poccard@gmail.com>
2021-03-09 10:01:09 +0100

    containers/Bills.js -> 71.43%

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,9 +1,11 @@
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import '@testing-library/jest-dom/extend-expect'
 
 import { localStorageMock } from "../__mocks__/localStorage.js"
+import Bills  from "../containers/Bills.js"
+import { ROUTES } from "../constants/routes"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -31,4 +33,48 @@ describe("Given I am connected as an employee", () => {
       expect(screen.getAllByText('Erreur')).toBeTruthy()
     })
   })
+  describe('When I click on the Create a new bill button', () => {
+		test('Then I should be sent to the NewBill page', () => {
+			Object.defineProperty(window, 'localStorage', {
+				value: localStorageMock,
+			})
+			window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }))
+			const html = BillsUI({ data: [] })
+			document.body.innerHTML = html
+			const onNavigate = (pathname) => {
+				document.body.innerHTML = ROUTES({ pathname })
+			}
+			const bills = new Bills({ document, onNavigate, firestore: null, localStorage: window.localStorage })
+			const button = screen.getByTestId('btn-new-bill')
+			const handleClickNewBill = jest.fn((e) => bills.handleClickNewBill(e))
+			button.addEventListener('click', handleClickNewBill)
+			fireEvent.click(button)
+			expect(handleClickNewBill).toHaveBeenCalled()
+			// const newBillForm = screen.getByTestId('form-new-bill')
+			// expect(newBillForm).toBeTruthy()
+		})
+	})
+	describe('When I click on the Eye Icon button', () => {
+		test('Then a modal should open', async () => {
+			Object.defineProperty(window, 'localStorage', {
+				value: localStorageMock,
+			})
+			window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }))
+			const html = BillsUI({ data: bills })
+			document.body.innerHTML = html
+			const onNavigate = (pathname) => {
+				document.body.innerHTML = ROUTES({ pathname })
+			}
+			const bill = new Bills({ document, onNavigate, firestore: null, localStorage: window.localStorage, })
+			$.fn.modal = jest.fn()
+			const button = screen.getAllByTestId('icon-eye')[0]
+			const handleClickIconEye = jest.fn((e) => {
+				e.preventDefault()
+				bill.handleClickIconEye(button)
+			})
+			button.addEventListener('click', handleClickIconEye)
+			fireEvent.click(button)
+			expect(handleClickIconEye).toHaveBeenCalled()
+		})
+	})
 })
